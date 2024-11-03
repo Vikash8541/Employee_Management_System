e06a7c0564222fd27c7125041f2c23c44a981acb
Vikash Sharma <vikash705805@gmail.com>
2024-11-03 21:17:23 +0530

    create data by chat gpt and set into localStorage and get into LocalStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+import { useEffect } from "react"
 import Login from "./components/Auth/Login"
 import AdminDashboard from "./components/Dashboard/AdminDashboard"
 import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard"
+import { getLocalStorage, setLocalStorage } from "./utilits/LocalStorage"
 
 const App = () => {
+
+  // Add useEffect to know the code LocalStorage code is working and also run parellel to use the localStorage.
+
+  useEffect(()=>{
+    setLocalStorage();
+    // getLocalStorage();
+  })
+
+
   return (
     <>
       <Login/>
-      <EmployeeDashboard/>
-      <AdminDashboard/>
+      {/* <EmployeeDashboard/>
+      <AdminDashboard/> */}
     </>
   )
 }
